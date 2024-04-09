3c42f4f32517ab0331c9208fe896f75110b26090
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-09 17:15:19 +0200

    debuggint task 100 and 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,7 @@
-export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }());
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const item of reportWithIterator) {
+    employees.push(item);
+  }
+  return employees.join(' | ');
 }
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-        return Object.keys(employeesList).length;
-    }
+      return Object.keys(employeesList).length;
+    },
   };
 }
