3ad982b5b2db2c1c1798b9a7ee8a5fcf83963763
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-09 04:29:08 +0200

    debuggint task 4 and 6

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments(...args) {
-  return args.length;
+export default function returnHowManyArguments(...hiThereMyNameIsAmir) {
+  return hiThereMyNameIsAmir.length;
 }
