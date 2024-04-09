e5dec8aa9945cf04793cecefc9cbc6d96a31044a
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-09 17:10:55 +0200

    debuggint task 11 and 12

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,11 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  let response = {}
-  let tempArray = []
-  for (const deparment in departmentName) {
-    for (const employee of employees) {
-      tempArray.append(employee);
-    }
-    response[`${deparment}`] = tempArray;
-  }
-  return response;
+  return {
+    departmentName: [
+      ...employees,
+    ],
+  };
 }
--- 0x00-ES6_basic/notes.js
+++ 0x00-ES6_basic/notes.js
@@ -77,3 +77,69 @@ console.log(nums.includes(2)); // true
 // import functions and stuff
 import getSumOfHoods from './5-spread-operator';
 let getSumOfHoods = new getSumOfHoods();
+
+
+// classes
+
+export class Animal {
+    constructor(type, legs) {
+        this.type = type;
+        this.legs = legs;
+    }
+
+    makeSound(sound = "loud noise") {
+        console.log(sound);
+    }
+
+    get metaData() {
+        return `Type: ${this.type}, Legs: ${this.legs}`;
+    }
+
+    static return10() {
+        return 10;
+    }
+}
+
+export class Cat extends Animal{
+    constructor(type, legs, tail) { //overload
+        super(type, legs);
+        this.tail = tail;
+    }
+    makeSound(sound = "aaaaa"){ //override
+        console.log(sound);
+    }
+}
+
+import { Animal }from `./notes.js`;
+
+const cat = new Animal("cat", 4);
+
+cat.legs = 16;
+console.log(cat.makeSound());
+Animal.return10();
+
+const apiUrl = "https://random.api.com/get/any/data/you/want";
+
+function getFirstThing() {
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json[0]);
+    }).catch((error) => {
+        console.log("failed to fetch:<");
+    })
+}
+getFirstThing();
+
+
+async function asyncFunction() {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    console.log(json[0]);
+}
+// unique values only.
+const example = new set([1,1,1,2,1,3,4,5,6,7,7,7])
+example.add(5); // if it is not at the set, it will be added
+example.delete(1); // will return bolean if it was successfully deleted
+example.clear()// clear the set
+example.has("hi"); // return false, if found return true;
