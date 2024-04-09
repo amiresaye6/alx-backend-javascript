bf90f672c5aa690188c9023f3c1d09308f9c5ce0
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-09 16:02:32 +0200

    debuggint task 8 and 9

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,11 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {...departmentName}
+  let response = {}
+  let tempArray = []
+  for (const deparment in departmentName) {
+    for (const employee of employees) {
+      tempArray.append(employee);
+    }
+    response[`${deparment}`] = tempArray;
+  }
+  return response;
 }
--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,13 +1,13 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject.js'; // eslint-disable-line import/extensions
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: (income) => {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
