394268c8d642732e6583341035beffb7a381fd1d
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-12 01:36:40 +0200

    task 5 and 100 solved

--- 0x02-ES6_classes/notes.js
+++ 0x02-ES6_classes/notes.js
@@ -7,6 +7,7 @@ class Rectangle {
         this._width = width;
         this._height = height;
         this._color = color;
+        this._numberOfRequestsForArea = 0
     }
 
     getArea () {
@@ -16,8 +17,65 @@ class Rectangle {
     printRectangle() {
         console.log(`width: ${this._width}, height: ${this._height}, color: ${this._color}`)
     }
+
+    // geters and setters worl like a proberity
+    get width() {
+        this._numberOfRequestsForArea += 1;
+        return this._width;
+    }
+
+    set width(newWidth) {
+        this._width = newWidth;
+    }
+
+    //static methos doesnt need an object to use it
+    static getAnyThing() {
+        console.log("static method that does nothing useful")
+    }
 }
 
 
 let myRectangle = new Rectangle();
 let myRectangle2 = new Rectangle();
+myRectangle.width = 1555;
+
+console.log(myRectangle.width);
+
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    sayHi() {
+        console.log("person sayes hi")
+    }
+}
+
+class Programmer extends Person {
+    constructor(name, age, coding) {
+        super(name, age);
+        this._coding = coding;
+    }
+    //polymorphism
+    sayHi() {
+        super.sayHi(); // you can call the parent method like this in the function
+        console.log("programmer sayes hi")
+    }
+
+
+}
+
+
+// priveate variables can be done by adding #
+class priveateClass {
+    #name = "";
+    #age = 20
+    constructor(name, age) {
+        this.#name = name;
+        this.#age = age;
+    }
+    #PrivateMethod(input) {
+        console.log(input);
+    }
+}
