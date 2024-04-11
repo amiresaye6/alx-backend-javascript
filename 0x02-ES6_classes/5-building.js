0fdcbe60fbaf2a474a9b548d197c6fb866e66f60
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-12 01:56:51 +0200

    solving task 5 ... 10

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,16 @@
 export default class Building {
-    constructor(sqft) {
-        if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-            throw Error('Class extending Building must override evacuationWarningMessage');
-          } 
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    set sqft(val) {
-        this._sqft = val;
-    }
-
-    
+  set sqft(val) {
+    this._sqft = val;
+  }
 }
