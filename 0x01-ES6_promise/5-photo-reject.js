920a8f7d6e2592efbb155fba8a10b8ec683d0de7
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-12 19:24:40 +0200

    fixitn eslint style

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,2 +1,3 @@
 export default function uploadPhoto(filename) {
-    return Promise.reject(Error(`${filename} cannot be processed`));}
+  return Promise.reject(Error(`${filename} cannot be processed`));
+}
