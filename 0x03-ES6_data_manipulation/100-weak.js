6fe69b2903d06894540e8f301907687f3901b7b9
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-15 23:03:57 +0200

    end of advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,5 @@
 export const weakMap = new WeakMap();
 
-// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
 export function queryAPI(endPoint) {
   let called = 0;
   if (weakMap.get(endPoint)) called = weakMap.get(endPoint);
