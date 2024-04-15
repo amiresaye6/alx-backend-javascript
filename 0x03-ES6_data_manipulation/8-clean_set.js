265e534041badc6365f4bea7d6ebb1f53d860e9b
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-15 23:12:24 +0200

    debugint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,23 @@
 export default function cleanSet(set, startString) {
-  let res = '';
-  if (startString === '') {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
-  const len = startString.length;
-  for (const element of set) {
-    if (element.startsWith(startString)) {
-      res += `${element.slice(len)}-`;
-    }
-  }
-  return res.slice(0, -1);
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
 }
+
+// export default function cleanSet(set, startString) {
+//   let res = '';
+//   if (startString === '') {
+//     return '';
+//   }
+//   const len = startString.length;
+//   for (const element of set) {
+//     if (element.startsWith(startString)) {
+//       res += `${element.slice(len)}-`;
+//     }
+//   }
+//   return res.slice(0, -1);
+// }
