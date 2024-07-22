cc53b8ccbfeab8b778d6a67c0f7a85aaeae9ce34
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-22 20:06:13 +0300

    eslint fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(msg) {
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Amir Alsayed <https://github.com/amiresaye6>
+ */
+const displayMessage = (msg) => {
     console.log(msg);
-}
+};
 
 module.exports = displayMessage;
