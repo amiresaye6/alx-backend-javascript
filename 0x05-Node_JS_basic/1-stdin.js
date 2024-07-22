5d36997415e1e10c0deaecccd262dc0f1ae1554b
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-22 20:54:03 +0300

    stdin, stdout using node.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
+});
