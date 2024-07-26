91bdde8340cfb81fd3c3368d2606b4cd176b0ad5
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-26 16:43:30 +0300

    chai tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,134 +1,133 @@
-const chai = require("chai");
-const expect = chai.expect;
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   describe('type == "SUM"', () => {
-    it('chai test: equal positive numbers', () => {
+    it('chai tests: equal positive numbers', () => {
       expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4);
     });
 
-    it('chai test: equal positive numbers (alternate)', () => {
+    it('chai tests: equal positive numbers (alternate)', () => {
       expect(calculateNumber('SUM', 2.3, 1.8)).to.equal(4);
     });
 
-    it('chai test: equal negative numbers', () => {
+    it('chai tests: equal negative numbers', () => {
       expect(calculateNumber('SUM', -2.0, -2.0)).to.equal(-4);
     });
 
-    it('chai test: equal negative numbers (alternate)', () => {
+    it('chai tests: equal negative numbers (alternate)', () => {
       expect(calculateNumber('SUM', -2.3, -1.8)).to.equal(-4);
     });
 
-    it('chai test: negative and positive numbers', () => {
+    it('chai tests: negative and positive numbers', () => {
       expect(calculateNumber('SUM', -2.0, 2.0)).to.equal(0);
     });
 
-    it('chai test: positive and negative numbers', () => {
+    it('chai tests: positive and negative numbers', () => {
       expect(calculateNumber('SUM', 2.0, -2.0)).to.equal(0);
     });
 
-    it('chai test: 0 and 0', () => {
+    it('chai tests: 0 and 0', () => {
       expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
     });
   });
 
   describe('type == "SUBTRACT"', () => {
-    it('chai test: equal positive numbers', () => {
+    it('chai tests: equal positive numbers', () => {
       expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.equal(0);
     });
 
-    it('chai test: equal positive numbers (alternate)', () => {
+    it('chai tests: equal positive numbers (alternate)', () => {
       expect(calculateNumber('SUBTRACT', 2.3, 1.8)).to.equal(0);
     });
 
-    it('chai test: equal negative numbers', () => {
+    it('chai tests: equal negative numbers', () => {
       expect(calculateNumber('SUBTRACT', -2.0, -2.0)).to.equal(0);
     });
 
-    it('chai test: equal negative numbers (alternate)', () => {
+    it('chai tests: equal negative numbers (alternate)', () => {
       expect(calculateNumber('SUBTRACT', -2.3, -1.8)).to.equal(0);
     });
 
-    it('chai test: negative and positive numbers', () => {
+    it('chai tests: negative and positive numbers', () => {
       expect(calculateNumber('SUBTRACT', -2.0, 2.0)).to.equal(-4.0);
     });
 
-    it('chai test: positive and negative numbers', () => {
+    it('chai tests: positive and negative numbers', () => {
       expect(calculateNumber('SUBTRACT', 2.0, -2.0)).to.equal(4.0);
     });
 
-    it('chai test: 0 and 0', () => {
+    it('chai tests: 0 and 0', () => {
       expect(calculateNumber('SUBTRACT', 0.0, 0.0)).to.equal(0);
     });
   });
 
   describe('type == "DIVIDE"', () => {
-    it('chai test: positive numbers', () => {
+    it('chai tests: positive numbers', () => {
       expect(calculateNumber('DIVIDE', 8.0, 2.0)).to.equal(4.0);
     });
 
-    it('chai test: numbers with different signs', () => {
+    it('chai tests: numbers with different signs', () => {
       expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.equal(-3.5);
     });
 
-    it('chai test: numbers with different signs (alternate)', () => {
+    it('chai tests: numbers with different signs (alternate)', () => {
       expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.equal(-3.5);
     });
 
-    it('chai test: negative numbers', () => {
+    it('chai tests: negative numbers', () => {
       expect(calculateNumber('DIVIDE', -7.0, -2.0)).to.equal(3.5);
     });
 
-    it('chai test: equal positive numbers', () => {
+    it('chai tests: equal positive numbers', () => {
       expect(calculateNumber('DIVIDE', 2.0, 2.0)).to.equal(1);
     });
 
-    it('chai test: equal negative numbers', () => {
+    it('chai tests: equal negative numbers', () => {
       expect(calculateNumber('DIVIDE', -2.0, -2.0)).to.equal(1);
     });
 
-    it('chai test: equal rounded up numbers', () => {
+    it('chai tests: equal rounded up numbers', () => {
       expect(calculateNumber('DIVIDE', 2.6, 3.0)).to.equal(1);
     });
 
-    it('chai test: equal rounded down numbers', () => {
+    it('chai tests: equal rounded down numbers', () => {
       expect(calculateNumber('DIVIDE', 2.4, 2.0)).to.equal(1);
     });
 
-    it('chai test: 0 and positive number', () => {
+    it('chai tests: 0 and positive number', () => {
       expect(calculateNumber('DIVIDE', 0.0, 5.0)).to.equal(0);
     });
 
-    it('chai test: 0 and negative number', () => {
-      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(0);
+    it('chai tests: 0 and negative number', () => {
+      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(-0);
     });
 
-    it('chai test: positive number and 0', () => {
+    it('chai tests: positive number and 0', () => {
       expect(calculateNumber('DIVIDE', 5.0, 0)).to.equal('Error');
     });
 
-    it('chai test: positive number and number rounded down to 0',() => {
+    it('chai tests: positive number and number rounded down to 0', () => {
       expect(calculateNumber('DIVIDE', 5.0, 0.2)).to.equal('Error');
     });
 
-    it('chai test: positive number and number rounded up to 0',() => {
+    it('chai tests: positive number and number rounded up to 0', () => {
       expect(calculateNumber('DIVIDE', 5.0, -0.2)).to.equal('Error');
     });
 
-    it('chai test: negative number and 0',() => {
+    it('chai tests: negative number and 0', () => {
       expect(calculateNumber('DIVIDE', -5.0, 0)).to.equal('Error');
     });
 
-    it('chai test: negative number and number rounded down to zero',() => {
+    it('chai tests: negative number and number rounded down to zero', () => {
       expect(calculateNumber('DIVIDE', -5.0, 0.2)).to.equal('Error');
     });
 
-    it('chai test: negative number and number rounded up to zero',() => {
+    it('chai tests: negative number and number rounded up to zero', () => {
       expect(calculateNumber('DIVIDE', -5.0, -0.2)).to.equal('Error');
     });
 
-    it('chai test: 0 and 0', () => {
+    it('chai tests: 0 and 0', () => {
       expect(calculateNumber('DIVIDE', 0.0, 0.0)).to.equal('Error');
     });
   });
