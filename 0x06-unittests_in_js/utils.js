ba069a34bca9ed031917d96fd56530b5f5ff8c55
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-26 18:07:05 +0300

    finally

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,13 +1,13 @@
 const Utils = {
   calculateNumber(type, a, b) {
-    if (type === "SUM") {
+    if (type === 'SUM') {
       return Math.round(a) + Math.round(b);
     }
-    if (type === "SUBTRACT") {
+    if (type === 'SUBTRACT') {
       return Math.round(a) - Math.round(b);
     }
-    if (type === "DIVIDE") {
-      return Math.round(b) === 0 ? "Error" : Math.round(a) / Math.round(b);
+    if (type === 'DIVIDE') {
+      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
     }
     return 0;
   },
